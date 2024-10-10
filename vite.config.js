1005ad0b01c48b49fef3df5fcf360a6d29f2926c
nondotxyz <shippudennaruto668@gmail.com>
2024-10-10 15:31:47 +0700

    feat: github deploy!

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/luck-be-a-landlord-editor",
   build: {
     sourcemap: true,
   }
